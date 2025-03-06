0ba2f412ca6c9bd179cd5b43eeb11e622416a321
Nguyen Sang Sinh <sangsinh789@gmail.com>
2025-03-06 20:44:31 +0700

    ajourter5(adapter): builder from interface to interface

---
+++ design-go-udemy/5-adapter/adapter-caching/main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+func minmax(a, b int) (int, int) {
+	if a < b {
+		return a, b
+	}
+	return b, a
+}
+
+type Line struct {
+	X1, Y1, X2, Y2 int
+}
+
+type VectorImage struct {
+	Lines []Line
+}
+
+func NewRectangle(width, height int) *VectorImage {
+	width -= 1
+	height -= 1
+	return &VectorImage{[]Line{
+		Line{0, 0, width, 0},
+		Line{0, 0, 0, height},
+		Line{width, 0, width, height},
+		Line{0, height, width, height},
+	}}
+}
+
+type Point struct {
+	X, Y int
+}
+
+type RasterImage interface {
+	GetPoints() []Point
+}
+
+func DrawPoints(owner RasterImage) string {
+	maxX, maxY := 0, 0
+	points := owner.GetPoints()
+	for _, pixel := range points {
+		if pixel.X > maxX {
+			maxX = pixel.X
+		}
+		if pixel.Y > maxY {
+			maxY = pixel.Y
+		}
+	}
+	maxX += 1
+	maxY += 1
+
+	// preallocate
+	data := make([][]rune, maxY)
+	for i := 0; i < maxY; i++ {
+		data[i] = make([]rune, maxX)
+		for j := range data[i] {
+			data[i][j] = ' '
+		}
+	}
+
+	for _, point := range points {
+		data[point.Y][point.X] = '*'
+	}
+
+	b := strings.Builder{}
+	for _, line := range data {
+		b.WriteString(string(line))
+		b.WriteRune('\n')
+	}
+
+	return b.String()
+}
+
+/*
+Problem here -> we want to create NewRectangle -> and DrawPoints from this rectangle
+But we can not DrawPoints from NewRectangle
+Solution:
+*/
+type vectorToRasterAdapter struct {
+	points []Point
+}
+
+// 16 byte using MD5 hashing
+var pointCache = map[[16]byte][]Point{}
+
+func (a *vectorToRasterAdapter) GetPoints() []Point {
+	return a.points
+}
+
+func (a *vectorToRasterAdapter) addLineHash(line Line) {
+	hash := func(object interface{}) [16]byte {
+		bytes, _ := json.Marshal(object)
+		return md5.Sum(bytes)
+	}
+
+	h := hash(line)
+	if pts, ok := pointCache[h]; ok {
+		a.points = append(a.points, pts...)
+		return
+	}
+	left, right := minmax(line.X1, line.X2)
+	top, bottom := minmax(line.Y1, line.Y2)
+	dx := right - left
+	dy := line.Y2 - line.Y1
+
+	if dx == 0 {
+		for y := top; y <= bottom; y++ {
+			a.points = append(a.points, Point{left, y})
+		}
+	} else if dy == 0 {
+		for x := left; x <= right; x++ {
+			a.points = append(a.points, Point{x, top})
+		}
+	}
+	pointCache[h] = a.points
+	fmt.Println("generated", len(a.points), "points")
+}
+
+func (a *vectorToRasterAdapter) addLine(line Line) {
+	left, right := minmax(line.X1, line.X2)
+	top, bottom := minmax(line.Y1, line.Y2)
+	dx := right - left
+	dy := line.Y2 - line.Y1
+
+	if dx == 0 {
+		for y := top; y <= bottom; y++ {
+			a.points = append(a.points, Point{left, y})
+		}
+	} else if dy == 0 {
+		for x := left; x <= right; x++ {
+			a.points = append(a.points, Point{x, top})
+		}
+	}
+	fmt.Println("generated", len(a.points), "points")
+}
+
+func VectorToRaster(vi *VectorImage) RasterImage {
+	adapter := vectorToRasterAdapter{}
+	for _, line := range vi.Lines {
+		adapter.addLineHash(line)
+	}
+	return &adapter
+}
+func main() {
+	rc := NewRectangle(6, 4)
+	a := VectorToRaster(rc)
+	b := VectorToRaster(rc)
+	fmt.Println(DrawPoints(a))
+	fmt.Println(DrawPoints(b))
+}
